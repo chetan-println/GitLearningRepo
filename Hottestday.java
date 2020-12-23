f94e48145142328e8a62b245b59d5ae49b903183
Chetan <mishrachetan001@gmail.com>
2020-12-23 13:21:10 +0530

    initial commit

---
+++ Hottestday.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import edu.duke.*;
+import org.apache.commons.csv.*;
+public class Hottestday
+{
+    public void hottesthour(CSVParser parser){
+        CSVRecord largestsofar=null;
+        for(CSVRecord record:parser){
+            if(largestsofar==null){
+                largestsofar=record;}
+                else{
+                    double largest=Double.parseDouble(largestsofar.get("TemperatureF"));
+                    double curr=Double.parseDouble(record.get("TemperatureF"));
+                if(largest<curr){
+                largestsofar=record;}}}
+                System.out.println("the largest temp is"+largestsofar.get("TemperatureF"));}
+                public void printMaxTemp(){
+                    FileResource fr=new FileResource();
+                    CSVParser parser=fr.getCSVParser();
+                    hottesthour(parser);}
+                    
+                    public static void main(String[] args){
+                        Hottestday hd=new Hottestday();
+                        hd.printMaxTemp();}
+
+}
